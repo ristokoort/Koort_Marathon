dfb1826d64dac2296efbf6b64bc647dcc75a3b0c
ristokoort <114060658+ristokoort@users.noreply.github.com>
2023-01-27 17:27:36 +0200

    Add project files.

---
+++ Koort_Marathon/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Koort_Marathon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
