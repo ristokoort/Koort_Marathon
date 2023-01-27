4d59e6233ae7f8707a0eafa9cbaab8f4761f2b7b
ristokoort <114060658+ristokoort@users.noreply.github.com>
2023-01-27 17:29:01 +0200

    Add a model Runner

---
+++ Koort_Marathon/Models/Runner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Koort_Marathon.Models
+{
+    public class Runner
+    {
+        //1,Toomas,Tamm,13.00,15.00,2
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime FinishTime { get; set; }
+        public int BreakTime { get; set; }
+    }
+}
