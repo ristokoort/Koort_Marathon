fe081234c80d94435f21f814a3939f21626b0a6a
ristokoort <114060658+ristokoort@users.noreply.github.com>
2023-01-27 17:30:18 +0200

    Add scaffolded RunnersController with Views

---
+++ Koort_Marathon/Data/Koort_MarathonContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Koort_Marathon.Models;
+
+namespace Koort_Marathon.Data
+{
+    public class Koort_MarathonContext : DbContext
+    {
+        public Koort_MarathonContext (DbContextOptions<Koort_MarathonContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Koort_Marathon.Models.Runner> Runner { get; set; }
+    }
+}
