de22a0f02332bde2a122f6fd768792892718cd94
ristokoort <114060658+ristokoort@users.noreply.github.com>
2023-03-19 20:48:08 +0200

    Add award model, migration

---
+++ Koort_Marathon/Migrations/20230319184740_AddAwardTable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Koort_Marathon.Migrations
+{
+    public partial class AddAwardTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
